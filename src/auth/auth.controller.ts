9cc14ac9a08b9dd7c973548fc85c0b3d058c4044
Shevchenko <daniil.shevchenko@sigmaukraine.com>
2024-06-10 11:27:25 +0300

    auth register endpoint. Added class-validation dep

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,32 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { Routes, Services } from 'src/utils/types';
 import { IAuthService } from './auth';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Controller(Routes.AUTH)
 export class AuthController {
   constructor(
     @Inject(Services.AUTH) private authService: IAuthService
   ) { }
+
+  @Post('register')
+  registerUser(
+    @Body() createuserdto: CreateUserDto
+  ) {
+    console.log(createuserdto);
+    return '1231'
+  }
+
+  @Post('login')
+  loginUser() { }
+
+  @Get('status')
+  status() {
+
+  }
+
+  @Post('logout')
+  logout() {
+
+  }
 }
